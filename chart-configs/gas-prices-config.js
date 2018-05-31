793f4523049f56566405408560bb0f19ee55d219
John Osterman <ostermanj@gmail.com>
2018-05-31 13:38:55 -0400

    Fourth chart is in

--- chart-configs/gas-prices-config.js
+++ chart-configs/gas-prices-config.js
@@ -1,16 +1,8 @@
-const dataSource = require('../data/sales.json');
-//const scrollmonitor = require('scrollmonitor');
-//import hcApp from '../dev-js/highchart-app.js';
+const dataSource = require('../data/gas-prices.json');
 import { sharedLineMethods } from '../dev-js/shared-line-methods.js';
 
 function customUpdate(isReplay){ // update function for this chart only
     console.log(this.Highchart.renderTo);
-
-  /*  var elementWatcher = scrollmonitor.create(this.Highchart.renderTo);
-    elementWatcher.fullyEnterViewport(() => {
-   //     animate.call(this);
-        elementWatcher.destroy();
-    });*/
 
     var replay = document.createElement('button');
     replay.classList.add('overlay-replay');
@@ -24,7 +16,7 @@ function customUpdate(isReplay){ // update function for this chart only
 
 
 
-    this.Highchart.setClassName('predicted-sales');
+    this.Highchart.setClassName('predicted-gas-prices');
     var togglableElements = this.Highchart.renderTo.querySelectorAll('.overlay-replay, .highcharts-title, .highcharts-subtitle, .highcharts-yaxis-grid, .highcharts-axis, .highcharts-axis-labels, .highcharts-legend, .highcharts-credits, .highcharts-note');
     togglableElements.forEach(el => {
         el.style.opacity = 0;
@@ -64,14 +56,14 @@ function customUpdate(isReplay){ // update function for this chart only
        this.Highchart.series[0].addPoint(this.dataSource[0]['2000']);
        this.Highchart.series[0].addPoint(this.dataSource[0]['2001']);
        this.Highchart.series[0].points[1].select(true, true);
-       annotateYear.call(this, 0, 2001, `In 2001, electricity sales totaled ${ Highcharts.numberFormat(this.dataSource[0]['2001'], 0) } billion kWh.`, 'left');
+       annotateYear.call(this, 0, 2001, `In 2001, natural gas prices averaged $${ Highcharts.numberFormat(this.dataSource[0]['2001'], 2) } per million Btu.`, 'left');
        setTimeout(() => {
 
            
             var timeoutDelay = 3000; // set back to 3000
             var step1 = animateSeries.call(this, 0, 2002, 2006).then(() => {
                 this.Highchart.annotations[0].setVisible(false);
-                annotateYear.call(this, 0, 2006, `By 2006, sales reached ${ Highcharts.numberFormat(this.dataSource[0]['2006'], 0) } billion kWh.`, 'left');
+                annotateYear.call(this, 0, 2006, `They peaked in 2005 at $${ Highcharts.numberFormat(this.dataSource[0]['2005'], 2) } per million Btu and then fell back to $${ Highcharts.numberFormat(this.dataSource[0]['2006'], 2) } in 2006.`, 'left');
                 setTimeout(() => {
                     backfillSeries.call(this, 1, 2006, 2009);//.then(() => {
                     this.Highchart.annotations[1].setVisible(false);
@@ -79,17 +71,16 @@ function customUpdate(isReplay){ // update function for this chart only
                     toggleLastPoint.call(this, 1);
                     setTimeout(() => {
                         toggleLastPoint.call(this, 1);
-                        this.Highchart.axes[1].setExtremes(3500,4300);
                         this.Highchart.annotations[2].setVisible(false);
 
                         
                             backfillSeries.call(this, 1, 2010, 2017);
                             toggleLastPoint.call(this, 1);
-                            annotateYear.call(this, 1, 2017, `It predicted sales increasing to ${ Highcharts.numberFormat(this.dataSource[1]['2017'], 0) } billion kWh in 2017.`, 'right');
+                            annotateYear.call(this, 1, 2017, `It predicted prices increasing slightly to $${ Highcharts.numberFormat(this.dataSource[1]['2017'], 2) } in 2017.`, 'right');
                             setTimeout(() => {
                                 toggleLastPoint.call(this, 0);
                                 this.Highchart.annotations[3].setVisible(false);
-                                annotateYear.call(this, 0, 2008, `But by 2008, sales had already begun to fall, partly in response to the Great Recession.`, 'left');
+                                annotateYear.call(this, 0, 2008, `In 2008 prices peaked again.`, 'right');
                                 animateSeries.call(this, 0, 2007, 2008).then(() => {
                                     setTimeout(() => {
                                         this.Highchart.annotations[4].setVisible(false);
@@ -101,32 +92,31 @@ function customUpdate(isReplay){ // update function for this chart only
                                             this.Highchart.annotations[5].setVisible(false);
                                             backfillSeries.call(this, 2, 2012, 2017);
                                             toggleLastPoint.call(this, 2);
-                                            annotateYear.call(this, 2, 2017, `It predicted sales increasing to ${ Highcharts.numberFormat(this.dataSource[2]['2017'], 0) } billion kWh in 2017.`, 'right');
+                                            annotateYear.call(this, 2, 2017, `It predicted gas prices falling slightly by 2017, to $${ Highcharts.numberFormat(this.dataSource[2]['2017'], 2) }.`, 'right');
                                             //toggleLastPoint.call(this, 2);
                                             setTimeout(() => {
                                                this.Highchart.annotations[6].setVisible(false); 
                                                toggleLastPoint.call(this,0);
                                                animateSeries.call(this, 0, 2009, 2014).then(() => {
-                                                   annotateYear.call(this, 0, 2014, `But by 2014, slow  sales had kept the actual numbers below the estimates.`, 'right');
+                                                   annotateYear.call(this, 0, 2014, `In 2014 gas prices were just under the 2011 estimate but well below the 2009 estimate.`, 'right');
                                                    setTimeout(() => {
                                                         this.Highchart.annotations[7].setVisible(false); 
                                                         backfillSeries.call(this, 3, 2014, 2016);
                                                         toggleLastPoint.call(this, 3);
-                                                        annotateYear.call(this, 3, 2016, `In 2016, yet another Annual Energy Outlook was released with projections pinned to 2014 numbers.`, 'right');
+                                                        annotateYear.call(this, 3, 2016, `In 2016, an additional Annual Energy Outlook was released with projections pinned to 2014 numbers.`, 'right');
                                                         setTimeout(() => {
                                                             toggleLastPoint.call(this,3);
                                                             this.Highchart.annotations[8].setVisible(false); 
                                                             animateSeries.call(this, 3, 2017, 2017).then(() => {
-                                                                annotateYear.call(this, 3, 2017, `This one predicted sales increasing slightly to  ${ Highcharts.numberFormat(this.dataSource[3]['2017'], 0) } billion kWh in 2017.`, 'right');
+                                                                annotateYear.call(this, 3, 2017, `It predicted prices increasing a little to $${ Highcharts.numberFormat(this.dataSource[3]['2017'], 2) } in 2017.`, 'right');
                                                                 setTimeout(() => {
                                                                     toggleLastPoint.call(this,0);
                                                                     this.Highchart.annotations[9].setVisible(false); 
                                                                     animateSeries.call(this,0, 2015, 2017).then(() => {
-                                                                        annotateYear.call(this, 0, 2017, `Sales, however, have gone down  since 2014.`, 'right');
+                                                                        annotateYear.call(this, 0, 2017, `Prices have remained low since then.`, 'right');
                                                                         setTimeout(() => {
                                                                             this.Highchart.annotations[10].setVisible(false); 
                                                                             this.Highchart.axes[0].setExtremes(2001,2035);
-                                                                            this.Highchart.axes[1].setExtremes(3500,5000);
                                                                             setTimeout(() => {
                                                                                 toggleLastPoint.call(this,1);
                                                                                 toggleLastPoint.call(this,2);
@@ -137,7 +127,7 @@ function customUpdate(isReplay){ // update function for this chart only
                                                                                 toggleLastPoint.call(this,1);
                                                                                 toggleLastPoint.call(this,2);
                                                                                 toggleLastPoint.call(this,3);
-                                                                                annotateYear.call(this, 2, 2035, 'The Annual Energy Outlook estimates have flattened over the years in response to lower-than-expected sales. That has consequences for estimates of future baseline emissions and the effects of carbon taxes.', 'left');
+                                                                                annotateYear.call(this, 2, 2035, 'The Annual Energy Outlook estimates have decreased considerably over time and predict that gas prices will remain low. Lower-than-expected gas prices have big consequences for estimates of future baseline emissions and the effects of carbon taxes.', 'right');
                                                                                 setTimeout(() => {
                                                                                     this.Highchart.annotations[11].setVisible(false); 
                                                                                     togglableElements.forEach(el => {
@@ -213,24 +203,25 @@ export default {
         }
     },
     subtitle: {
-        text: 'Estimates have flattened over the years as actual demand has fallen short of expectations.'
+        text: 'Estimates have decreased over the years and now predict that low prices will continue.'
     },           
     title: {
-        text: 'U.S. electricity demand, actual and estimated',
+        text: 'U.S. natural gas prices to the electric power sector, actual and estimated',
     },
     tooltip: {
         valueDecimals: 2,
-        valueSuffix: ' TWh'
+        valueSuffix: ' per million Btu',
+        valuePrefix: '$'
     },
     xAxis: {
         min: 2001,
         max: 2018
     },
     yAxis: {
-        max: 4000,
-        min: 3500,
+        max: 10,
+        min: 0,
         title: {
-            text: 'terawatt hours',
+            text: '$(2011) per million Btu',
             align:'high',
             rotation: 0,
             margin:0,
@@ -245,5 +236,5 @@ export default {
     seriesCreator: sharedLineMethods.createSeries,
     updateFunction: customUpdate,
     initialUpdateParams: [],
-    note: '1 terawatt hour = 1 billion kilowatt hours. Sources: U.S. Energy Information Administration (EIA), Annual Energy Outlook (2009, 2011, and 2016); EIA Monthly Energy Review, April 2018.',
+    note: 'Sources: U.S. Energy Information Administration (EIA), Annual Energy Outlook (2009, 2011, and 2016); EIA Monthly Energy Review, April 2018.',
 };
--- dev-js/index.js
+++ dev-js/index.js
@@ -1,5 +1,5 @@
 import HighchartApp from './highchart-app.js';
-var chartConfigs = [require('../chart-configs/generation-config.js').default, require('../chart-configs/decomposition-config.js').default, require('../chart-configs/sales-config.js').default];
+var chartConfigs = [require('../chart-configs/generation-config.js').default, require('../chart-configs/decomposition-config.js').default, require('../chart-configs/sales-config.js').default, require('../chart-configs/gas-prices-config.js').default];
 console.log(HighchartApp, chartConfigs);
 
 HighchartApp.chartController.init(chartConfigs);    
